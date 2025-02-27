1b6be0377fbf77419a4430fbe54d1d26e69e3e40
KieSun <zx597813039@gmail.com>
2025-02-27 08:06:47 +0800

    feat: Add initial Communities component structure

---
+++ app/components/Communities.tsx
@@ -0,0 +1,12 @@
+export default function Communities() {
+  return (
+    <div>
+      <div>
+        <div>
+          <span>Communities</span>
+          <span>Newest Communities</span>
+        </div>
+      </div>
+    </div>
+  );
+}
