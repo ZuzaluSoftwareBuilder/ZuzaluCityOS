e3097068c9b79258c9bb0d58caba175e4338868c
KieSun <zx597813039@gmail.com>
2025-03-02 09:13:03 +0800

    refactor: Enhance Sidebar with dynamic tabs and improved layout
    - Added Tabs component to switch between Events and Communities sections
    - Restructured sidebar layout with more compact and focused design
    - Updated footer links and open-source section styling
    - Replaced ZuButton with new Button component
    - Improved event list rendering and hover interactions

--- components/layout/Sidebar/index.tsx
+++ components/layout/Sidebar/index.tsx
@@ -2,8 +2,6 @@
 import * as React from 'react';
 import { usePathname, useRouter } from 'next/navigation';
 import {
-  EventIcon,
-  SpaceIcon,
   HomeIcon,
   ArrowUpRightIcon,
   ShapeIcon,
@@ -13,11 +11,11 @@ import {
   VideoIcon,
 } from 'components/icons';
 import { useCeramicContext } from '@/context/CeramicContext';
-import { ZuButton } from '@/components/core';
 import { EventData, Event } from '@/types';
 import { useCallback, useEffect, useState } from 'react';
 import Image from 'next/image';
-import { cn } from '@heroui/react';
+import { cn, Tab, Tabs } from '@heroui/react';
+import { Button } from '@/components/base';
 
 interface SidebarProps {
   selected: string;
@@ -62,6 +60,41 @@ const naviButtons = [
   },
 ];
 
+const footerItems = [
+  {
+    content: 'Docs',
+    url: 'https://bit.ly/ZuSoftware-doc',
+  },
+  {
+    content: 'About',
+    url: 'https://zuzalu.gitbook.io/zuzalu-beta-docs/welcome/zuzalu.city-vision-and-mission',
+  },
+  {
+    content: 'X/Twitter',
+    url: 'https://x.com/Zuzalu_city',
+  },
+  {
+    content: 'Farcaster',
+    url: 'https://warpcast.com/zuzalucity',
+  },
+  {
+    content: 'Builder Chat',
+    url: 'https://matrix.to/#/#zuzalusoftware:matrix.org',
+  },
+  {
+    content: 'Medium',
+    url: 'https://medium.com/@Zuzalu_city',
+  },
+  {
+    content: 'Paragraph',
+    url: 'https://paragraph.xyz/@zuzalu.city',
+  },
+  {
+    content: 'Podcast',
+    url: 'https://youtube.com/@ZuzaluCity',
+  },
+];
+
 const Sidebar: React.FC<SidebarProps> = ({ selected }) => {
   const router = useRouter();
   const pathname = usePathname();
@@ -69,6 +102,7 @@ const Sidebar: React.FC<SidebarProps> = ({ selected }) => {
 
   const [events, setEvents] = useState<Event[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [selectedTab, setSelectedTab] = useState('events');
 
   const getEvents = async () => {
     try {
@@ -153,41 +187,6 @@ const Sidebar: React.FC<SidebarProps> = ({ selected }) => {
     isAuthenticated && fetchData();
   }, [ceramic?.did?.parent, isAuthenticated]);
 
-  const footerItems = [
-    {
-      content: 'Docs',
-      url: 'https://bit.ly/ZuSoftware-doc',
-    },
-    {
-      content: 'About',
-      url: 'https://zuzalu.gitbook.io/zuzalu-beta-docs/welcome/zuzalu.city-vision-and-mission',
-    },
-    {
-      content: 'X/Twitter',
-      url: 'https://x.com/Zuzalu_city',
-    },
-    {
-      content: 'Farcaster',
-      url: 'https://warpcast.com/zuzalucity',
-    },
-    {
-      content: 'Builder Chat',
-      url: 'https://matrix.to/#/#zuzalusoftware:matrix.org',
-    },
-    {
-      content: 'Medium',
-      url: 'https://medium.com/@Zuzalu_city',
-    },
-    {
-      content: 'Paragraph',
-      url: 'https://paragraph.xyz/@zuzalu.city',
-    },
-    {
-      content: 'Podcast',
-      url: 'https://youtube.com/@ZuzaluCity',
-    },
-  ];
-
   const handleClick = useCallback(
     (item: any) => {
       !item.isSoon && router.push(item.url);
@@ -195,107 +194,142 @@ const Sidebar: React.FC<SidebarProps> = ({ selected }) => {
     [router],
   );
 
+  const handleTabChange = useCallback((key: React.Key) => {
+    setSelectedTab(key as string);
+  }, []);
+
   return (
     <div
       className={`${selected !== 'Space Details' ? 'w-[260px]' : 'w-auto'} h-[calc(100vh-50px)] sticky top-[50px] transition-[width] duration-300 ease-in-out bg-[rgba(34,34,34,0.9)] flex flex-col border-r border-[#383838]`}
     >
-      <div className="flex flex-col p-[10px] gap-[10px]">
-        {naviButtons.map((item, index) => {
-          return (
-            <div
-              className={cn(
-                'flex p-[8px_10px] items-center gap-[10px] rounded-[10px] justify-between group transition-all duration-300 ease-in-out',
-                !item.isSoon ? 'cursor-pointer hover:bg-[#383838]' : 'opacity-70',
-                pathname === item.url ? 'bg-[#383838]' : 'bg-transparent',
-              )}
-              onClick={() => handleClick(item)}
-              key={index}
-            >
-              <div className="flex flex-row items-center gap-[10px]">
-                <div
-                  className={cn(
-                    'flex flex-row items-center gap-[10px] opacity-60 transition-opacity duration-300 ease-in-out',
-                    !item.isSoon ? 'group-hover:opacity-100' : 'opacity-20',
-                    pathname === item.url && 'opacity-100',
-                  )}
-                >
-                  {item.icon}
-                  {selected !== 'Space Details' && (
-                    <p className="text-white font-medium text-[14px]">
-                      {item.content}
+      <div className="flex-1 overflow-y-auto">
+        <div className="flex flex-col p-[10px] gap-[10px]">
+          {naviButtons.map((item, index) => {
+            return (
+              <div
+                className={cn(
+                  'flex p-[8px_10px] items-center gap-[10px] rounded-[10px] justify-between group transition-all duration-300 ease-in-out',
+                  !item.isSoon
+                    ? 'cursor-pointer hover:bg-[#383838]'
+                    : 'opacity-70',
+                  pathname === item.url ? 'bg-[#383838]' : 'bg-transparent',
+                )}
+                onClick={() => handleClick(item)}
+                key={index}
+              >
+                <div className="flex flex-row items-center gap-[10px]">
+                  <div
+                    className={cn(
+                      'flex flex-row items-center gap-[10px] opacity-60 transition-opacity duration-300 ease-in-out',
+                      !item.isSoon ? 'group-hover:opacity-100' : 'opacity-20',
+                      pathname === item.url && 'opacity-100',
+                    )}
+                  >
+                    {item.icon}
+                    {selected !== 'Space Details' && (
+                      <p className="text-white font-medium text-[14px]">
+                        {item.content}
+                      </p>
+                    )}
+                  </div>
+                  {item.isNew && (
+                    <p className="rounded-[4px] bg-[rgba(125,255,209,0.10)] p-[2px_4px] text-[12px] text-[#7dffd1] font-semibold leading-[1.4]">
+                      New!
                     </p>
                   )}
                 </div>
-                {item.isNew && (
-                  <p className="rounded-[4px] bg-[rgba(125,255,209,0.10)] p-[2px_4px] text-[12px] text-[#7dffd1] font-semibold leading-[1.4]">
-                    New!
+                {item.isSoon ? (
+                  <p className="text-[10px] leading-[1.2] opacity-50 text-white">
+                    SOON
                   </p>
-                )}
+                ) : item.version ? (
+                  <p className="text-[10px] leading-[1.2] opacity-30 text-white">
+                    {item.version}
+                  </p>
+                ) : null}
               </div>
-              {item.isSoon ? (
-                <p className="text-[10px] leading-[1.2] opacity-50 text-white">
-                  SOON
-                </p>
-              ) : item.version ? (
-                <p className="text-[10px] leading-[1.2] opacity-30 text-white">
-                  {item.version}
-                </p>
-              ) : null}
-            </div>
-          );
-        })}
-      </div>
-      <div
-        className={`flex flex-col gap-[10px] border-t border-b border-[#383838] mx-[10px] py-[20px_0_10px] flex-1 ${isAuthenticated ? 'flex-col' : 'flex-col-reverse'} justify-between`}
-      >
-        {isAuthenticated ? (
-          <>
-            <p className="text-[10px] text-[rgba(255,255,255,0.7)] pl-[6px]">
-              YOUR RSVP&apos;D EVENTS
-            </p>
-            <div className="flex flex-col gap-[10px] overflow-y-auto max-h-[calc(100vh-492px)] flex-1">
-              {isLoading
-                ? Array.from({ length: 3 }).map((_, index) => (
+            );
+          })}
+        </div>
+        <div
+          className={`flex flex-col gap-[5px] border-t border-b-w-10 rounded-[10px] mx-[10px] py-[10px] justify-between`}
+        >
+          <p className="text-[10px] text-[rgba(255,255,255,0.7)] leading-[17px] text-center opacity-70 uppercase">
+            YOUR activities
+          </p>
+          <Tabs
+            aria-label="Tabs variants"
+            variant="light"
+            selectedKey={selectedTab}
+            onSelectionChange={handleTabChange}
+            className="w-full flex justify-center"
+            classNames={{
+              cursor: 'bg-white/5 shadow-none',
+              tabList: 'w-full',
+              tabContent: 'text-[14px] group-data-[selected=false]:opacity-50',
+            }}
+          >
+            <Tab key="events" title="Events" />
+            <Tab key="communities" title="Communities" />
+          </Tabs>
+          <div className="flex flex-col gap-[10px]">
+            {isLoading
+              ? Array.from({ length: 3 }).map((_, index) => (
+                  <div
+                    key={index}
+                    className="flex items-center gap-[10px] p-[6px_10px] opacity-70 cursor-pointer"
+                  >
+                    <div className="w-[20px] h-[20px] bg-gray-300 rounded animate-pulse"></div>
+                    <div className="w-[190px] h-[17px] bg-gray-300 rounded animate-pulse"></div>
+                  </div>
+                ))
+              : events.map((event, index) => {
+                  return (
                     <div
                       key={index}
-                      className="flex items-center gap-[10px] p-[6px_10px] opacity-70 cursor-pointer"
+                      className="flex items-center gap-[10px] p-[6px_10px] opacity-70 cursor-pointer hover:bg-[rgba(255,255,255,0.05)] rounded-[10px]"
+                      onClick={() => {
+                        router.push(`/events/${event.id}`);
+                      }}
                     >
-                      <div className="w-[20px] h-[20px] bg-gray-300 rounded animate-pulse"></div>
-                      <div className="w-[190px] h-[17px] bg-gray-300 rounded animate-pulse"></div>
-                    </div>
-                  ))
-                : events.map((event, index) => {
-                    return (
-                      <div
-                        key={index}
-                        className="flex items-center gap-[10px] p-[6px_10px] opacity-70 cursor-pointer hover:bg-[rgba(255,255,255,0.05)] rounded-[4px]"
-                        onClick={() => {
-                          router.push(`/events/${event.id}`);
+                      <Image
+                        src={event.imageUrl!}
+                        alt={event.title}
+                        width={20}
+                        height={20}
+                        style={{
+                          objectFit: 'cover',
+                          borderRadius: '2px',
+                          height: '20px',
+                          width: '20px',
                         }}
-                      >
-                        <Image
-                          src={event.imageUrl!}
-                          alt={event.title}
-                          width={20}
-                          height={20}
-                          style={{
-                            objectFit: 'cover',
-                            borderRadius: '2px',
-                            height: '20px',
-                            width: '20px',
-                          }}
-                        />
-                        <p className="text-white text-[14px] truncate">
-                          {event.title}
-                        </p>
-                      </div>
-                    );
-                  })}
-            </div>
-          </>
-        ) : null}
+                      />
+                      <p className="text-white text-[14px] font-semibold leading-[1.6] truncate">
+                        {event.title}
+                      </p>
+                    </div>
+                  );
+                })}
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col p-[10px] flex-shrink-0 border-t border-[#383838] gap-[10px]">
+        <div className="flex flex-row flex-wrap gap-[10px] p-[10px]">
+          {footerItems.map((item, index) => {
+            return (
+              <a
+                key={index}
+                className="text-[rgba(225,225,225,0.7)] text-[13px] no-underline hover:underline hover:decoration-[#7dffd1] hover:text-[#7dffd1] hover:opacity-70"
+                href={item.url}
+                target="_blank"
+              >
+                {item.content}
+              </a>
+            );
+          })}
+        </div>
         <div
-          className="flex gap-[10px] items-center justify-center cursor-pointer"
+          className="flex gap-[10px] items-center justify-center cursor-pointer opacity-70 hover:opacity-100"
           onClick={() =>
             window.open(
               'https://github.com/ZuzaluSoftwareBuilder/ZuzaluCityOS',
@@ -303,49 +337,22 @@ const Sidebar: React.FC<SidebarProps> = ({ selected }) => {
             )
           }
         >
-          <p className="text-[12px] text-[rgba(255,255,255,0.7)]">
-            Zuzalu.city is open source
+          <p className="text-[10px] text-white leading-[1.2] uppercase">
+            ZUZalu city is open source
           </p>
           <Image
             src="/sidebar/gitHub.png"
             alt="github"
             width={24}
             height={24}
-            style={{ opacity: 0.7 }}
           />
         </div>
-      </div>
-      <div className="flex flex-col mx-[10px] py-[10px_0]">
-        <div className="flex flex-col gap-[10px] pl-[10px]">
-          <div className="flex flex-row flex-wrap gap-[10px]">
-            {footerItems.map((item, index) => {
-              return (
-                <a
-                  key={index}
-                  className="text-[rgba(225,225,225,0.7)] text-[14px] no-underline hover:underline hover:decoration-[#7dffd1] hover:text-[#7dffd1] hover:opacity-70"
-                  href={item.url}
-                  target="_blank"
-                >
-                  {item.content}
-                </a>
-              );
-            })}
-          </div>
-        </div>
-        <ZuButton
-          variant="outlined"
-          endIcon={<ArrowUpRightIcon size={5} />}
-          sx={{
-            width: '100%',
-            height: '32px',
-            border: '1px solid rgba(255, 255, 255, 0.1)',
-            fontSize: '13px !important',
-            color: '#fff',
-            marginTop: '10px',
-            fontWeight: 400,
-            backgroundColor: 'transparent',
-          }}
-          onClick={() =>
+        <Button
+          variant="light"
+          border
+          className="text-[13px] leading-[1.4] p-[6px_14px]"
+          endContent={<ArrowUpRightIcon size={5} />}
+          onPress={() =>
             window.open(
               'https://zuzapp-v1-events-app-delta.vercel.app/dashboard/home',
               '_blank',
@@ -353,7 +360,7 @@ const Sidebar: React.FC<SidebarProps> = ({ selected }) => {
           }
         >
           Legacy Registry App
-        </ZuButton>
+        </Button>
       </div>
     </div>
   );
