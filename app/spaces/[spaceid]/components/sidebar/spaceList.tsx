39c8b3286c788255f785a9df5a33cbe47a6ac106
LGC <lguangcong@163.com>
2025-03-11 12:36:42 +0800

    feat(spaceLayout): add tooltip to space avatar

--- app/spaces/[spaceid]/components/sidebar/spaceList.tsx
+++ app/spaces/[spaceid]/components/sidebar/spaceList.tsx
@@ -8,7 +8,8 @@ import { isUserAssociated } from '@/utils/permissions';
 import { useCeramicContext } from '@/context/CeramicContext';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { Image } from '@heroui/react';
+import { Image, Tooltip } from '@heroui/react';
+import { divIcon } from 'leaflet';
 
 const SpaceList = () => {
   const [selected, setSelected] = useState('Spaces');
@@ -58,26 +59,34 @@ const SpaceItem = ({ space }: { space: Space }) => {
   const isActive = pathname.includes(`/spaces/${space.id}`);
 
   return (
-    <Link href={`/spaces/${space.id}`} className="cursor-pointer">
-      <div
-        className={`
+    <Tooltip placement="right" classNames={{
+      base: ['bg-transparent'],
+      content: [
+        'px-2.5 py-1 bg-[rgba(44,44,44,0.8)] border border-[rgba(255,255,255,0.1)] rounded-lg text-white text-sm'
+      ]
+    }}
+      content={space.name} >
+      <Link href={`/spaces/${space.id}`} className="cursor-pointer">
+        <div
+          className={`
           w-10 h-10 box-content rounded-full flex items-center justify-center cursor-pointer
           hover:shadow-[0_0_0_2px_rgba(255,255,255,0.2)]
           ${isActive ? 'border-2 border-[#2C2C2C] shadow-[0_0_0_2px_rgba(255,255,255,0.9)] hover:shadow-[0_0_0_2px_rgba(255,255,255,0.9)]' : 'border-none'}
         `}
-      >
-        {space.avatar ? (
-          <Image
-            src={space.avatar}
-            alt={space.name}
-            width={40}
-            height={40}
-            className="rounded-full object-cover"
-          />
-        ) : (
-          <div className="w-10 h-10 rounded-full bg-[#363636]" />
-        )}
-      </div>
-    </Link>
+        >
+          {space.avatar ? (
+            <Image
+              src={space.avatar}
+              alt={space.name}
+              width={40}
+              height={40}
+              className="rounded-full object-cover"
+            />
+          ) : (
+            <div className="w-10 h-10 rounded-full bg-[#363636]" />
+          )}
+        </div>
+      </Link>
+    </Tooltip>
   );
 };
