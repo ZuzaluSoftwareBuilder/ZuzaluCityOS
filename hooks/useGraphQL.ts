5804357ba710070a969a127cae8d441cd7c13a9f
KieSun <zx597813039@gmail.com>
2025-03-21 21:14:24 +0800

    refactor: enhance useGraphQL hook with options parameter
    - Updated the `useGraphQL` hook in `hooks/useGraphQL.ts` to accept an optional `UseQueryOptions` parameter, improving flexibility in query execution.
    - Refactored the query function to handle the new options parameter while maintaining type safety and clarity in GraphQL operations.
    - Simplified the handling of query variables for better usability and integration with React Query.

--- hooks/useGraphQL.ts
+++ hooks/useGraphQL.ts
@@ -1,18 +1,28 @@
 import { type TypedDocumentNode } from '@graphql-typed-document-node/core';
-import { useQuery, type UseQueryResult } from '@tanstack/react-query';
+import {
+  useQuery,
+  type UseQueryResult,
+  type UseQueryOptions,
+} from '@tanstack/react-query';
 import { useCeramicContext } from '@/context/CeramicContext';
 
 export function useGraphQL<TResult, TVariables>(
   document: TypedDocumentNode<TResult, TVariables>,
-  ...[variables]: TVariables extends Record<string, never> ? [] : [TVariables]
+  ...[variables, options]: TVariables extends Record<string, never>
+    ? [Omit<UseQueryOptions<TResult>, 'queryKey' | 'queryFn'>?]
+    : [TVariables, Omit<UseQueryOptions<TResult>, 'queryKey' | 'queryFn'>?]
 ): UseQueryResult<TResult> {
   const { composeClient } = useCeramicContext();
+
+  const queryKey = [(document as any).definitions?.[0]?.name?.value, variables];
+
   return useQuery({
-    queryKey: [(document as any).definitions?.[0]?.name?.value, variables],
+    queryKey,
     queryFn: async ({ queryKey }) =>
       composeClient.executeQuery(
         document,
-        queryKey[1] ? queryKey[1] : undefined,
+        queryKey[1] ? (queryKey[1] as Record<string, unknown>) : undefined,
       ),
+    ...options,
   });
 }
