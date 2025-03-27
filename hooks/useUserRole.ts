4d018d291503542e7fa93b465d19051d3f1c35e3
KieSun <zx597813039@gmail.com>
2025-03-27 08:21:08 +0800

    feat: integrate BuildInRoleContext for user role management
    - Added BuildInRoleContext to manage built-in user roles (follower, admin, owner, member).
    - Updated RootLayout to include BuildInRoleProvider for role context accessibility.
    - Refactored useUserRole hook to utilize the new BuildInRoleContext for fetching follower role ID.
    - Modified API member add route to conditionally include spaceId based on resource type.

--- hooks/useUserRole.ts
+++ hooks/useUserRole.ts
@@ -1,12 +1,13 @@
 import { useCeramicContext } from '@/context/CeramicContext';
 import { useGraphQL } from '@/hooks/useGraphQL';
 import { GET_USER_ROLES_QUERY } from '@/services/graphql/role';
-import { useQuery } from '@tanstack/react-query';
-import { supabase } from '@/utils/supabase/client';
+import { useBuildInRole } from '@/context/BuildInRoleContext';
 
 const useUserRole = () => {
   const { profile } = useCeramicContext();
   const userDId = profile?.author?.id;
+
+  const { followerRole } = useBuildInRole();
 
   const {
     data: userRoles,
@@ -28,22 +29,12 @@ const useUserRole = () => {
     },
   );
 
-  const { data: followerRoleId } = useQuery({
-    queryKey: ['getFollowerRoleId'],
-    queryFn: () => {
-      return supabase.from('role').select('*').eq('level', 'follower');
-    },
-    select: (data: any) => {
-      return data.data.id;
-    },
-  });
-
   return {
     userRoles,
     isUserRoleLoading,
     isUserRoleFetched,
-    followerRoleId
-  }
-}
+    followerRoleId: followerRole?.id,
+  };
+};
 
-export default useUserRole
+export default useUserRole;
