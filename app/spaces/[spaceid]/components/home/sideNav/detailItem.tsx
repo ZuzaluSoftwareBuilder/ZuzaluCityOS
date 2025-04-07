75f66ce49abdbc1cb0303d2be1487ae516d69211
LGC <lguangcong@163.com>
2025-04-07 20:23:39 +0800

    feat: enhance tags style

--- app/spaces/[spaceid]/components/home/sideNav/detailItem.tsx
+++ app/spaces/[spaceid]/components/home/sideNav/detailItem.tsx
@@ -11,11 +11,11 @@ const DetailItem = ({
 }) => {
   return (
     <div
-      className={`flex items-center justify-between border-b border-[rgba(255,255,255,0.1)] pb-[10px] text-[14px] leading-[1.6] text-white ${noBorderBottom ? 'border-none' : ''}`}
+      className={`flex items-center justify-between gap-[10px] border-b border-[rgba(255,255,255,0.1)] pb-[10px] text-[14px] leading-[1.6] text-white ${noBorderBottom ? 'border-none' : ''}`}
     >
-      <span>{title}</span>
+      <span className="shrink-0">{title}</span>
       {value ? (
-        <span>{value}</span>
+        <span className="text-right">{value}</span>
       ) : (
         <Skeleton className="h-[22px] w-[50px]" />
       )}
