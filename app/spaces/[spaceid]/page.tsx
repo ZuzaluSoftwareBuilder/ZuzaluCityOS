3129ff620b343aa52e8e3e9504564269c3e101e3
KieSun <zx597813039@gmail.com>
2025-04-09 13:39:08 +0800

    refactor: remove console logs from SpaceHomePage component
    - Eliminated unnecessary console log statements in useEffect for spaceData and eventsData to clean up the code and improve performance.

--- app/spaces/[spaceid]/page.tsx
+++ app/spaces/[spaceid]/page.tsx
@@ -5,7 +5,7 @@ import SpaceSection from './components/home/spaceSection';
 import { useGraphQL } from '@/hooks/useGraphQL';
 import { GET_SPACE_AND_EVENTS_QUERY_BY_ID } from '@/services/graphql/space';
 import { Event, Space } from '@/types';
-import React, { useEffect, useMemo } from 'react';
+import React, { useMemo } from 'react';
 import SpaceEventList from '@/app/spaces/[spaceid]/components/home/spaceEventList';
 import SideNav from '@/app/spaces/[spaceid]/components/home/sideNav/sideNav';
 import { useBuildInRole } from '@/context/BuildInRoleContext';
@@ -49,15 +49,6 @@ const SpaceHomePage: React.FC = () => {
   const eventsData = useMemo(() => {
     return (spaceData?.events?.edges.map((edge) => edge.node) || []) as Event[];
   }, [spaceData]);
-
-  useEffect(() => {
-    if (spaceData) {
-      console.log('spaceData', spaceData);
-    }
-    if (eventsData.length > 0) {
-      console.log('eventsData', eventsData);
-    }
-  }, [spaceData, eventsData]);
 
   return (
     <div className="flex h-full">
