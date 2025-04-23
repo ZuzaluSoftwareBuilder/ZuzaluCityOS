d71f6a1855421745750c5777ff418a1486f3bba2
KieSun <zx597813039@gmail.com>
2025-04-23 21:41:58 +0800

    refactor: replace GraphQL query with useQuery for space data retrieval and enhance error handling in SpaceList component

--- app/spaces/components/list.tsx
+++ app/spaces/components/list.tsx
@@ -1,5 +1,5 @@
 import { SpaceCard, SpaceCardSkeleton } from '@/components/biz/space/SpaceCard';
-import { Space } from '@/types';
+import { Space } from '@/models/space';
 import { useMemo, useState } from 'react';
 
 import ExploreSearch from '@/components/layout/explore/exploreSearch';
@@ -8,47 +8,33 @@ import {
   ResponsiveGridItem,
 } from '@/components/layout/explore/responsiveGridItem';
 import { useBuildInRole } from '@/context/BuildInRoleContext';
-import { useGraphQL } from '@/hooks/useGraphQL';
+import { useRepositories } from '@/context/RepositoryContext';
 import useUserSpace from '@/hooks/useUserSpace';
-import { GET_ALL_SPACE_AND_MEMBER_QUERY } from '@/services/graphql/space';
 import dayjs from '@/utils/dayjs';
 import { supabase } from '@/utils/supabase/client';
 import { useQuery } from '@tanstack/react-query';
-import { getSpaceRepository } from 'repositories/space';
 
 const SpaceList = () => {
   const [searchVal, setSearchVal] = useState<string>('');
 
   const { userJoinedSpaceIds, userFollowedSpaceIds } = useUserSpace();
-
+  const { spaceRepository } = useRepositories();
   const { adminRole, memberRole, isRoleLoading } = useBuildInRole();
 
-  const { data: spaces, isLoading } = useGraphQL(
-    // todo 这里涉及role，之后需要替换
-    ['GET_ALL_SPACE_AND_MEMBER_QUERY'],
-    GET_ALL_SPACE_AND_MEMBER_QUERY,
-    {
-      first: 100,
-      userRolesFilters: {
-        where: {
-          roleId: {
-            in: [adminRole, memberRole].map((role) => role?.id ?? ''),
-          },
-        },
-      },
+  const { data: spaces, isLoading } = useQuery({
+    queryKey: ['GET_ALL_SPACE_AND_MEMBER_QUERY'],
+    queryFn: () => {
+      return spaceRepository.getAllAndMembers(
+        [adminRole, memberRole].map((role) => role?.id ?? ''),
+      );
     },
-    {
-      select: (data) => {
-        if (!data?.data?.zucitySpaceIndex?.edges) {
-          return [];
-        }
-        return data.data.zucitySpaceIndex.edges.map(
-          (edge) => edge!.node,
-        ) as Space[];
-      },
-      enabled: !!adminRole && !!memberRole,
+    select: (data) => {
+      if (data.error) {
+        return [];
+      }
+      return data.data;
     },
-  );
+  });
 
   const { data: legacySpacesData, isLoading: isLegacyLoading } = useQuery({
     queryKey: ['GET_LEGACY_SPACES_DATA'],
@@ -65,36 +51,20 @@ const SpaceList = () => {
       })) as Space[];
     },
   });
-  // 临时显示supabase的,之后要换掉GET_ALL_SPACE_AND_MEMBER_QUERY
-  const { data: supabaseData, isLoading: isSupbaseDataLoading } = useQuery({
-    queryKey: ['GET_SUPABASE_SPACES_DATA'],
-    queryFn: () => {
-      return getSpaceRepository().getAll();
-    },
-    select: (data: any) => {
-      if (!data.data) {
-        return [];
-      }
-      return data.data.map((item: any) => ({
-        ...item,
-      })) as Space[];
-    },
-  });
 
   const filteredSpacesData = useMemo(() => {
     const sortedData = (spaces || [])
       .sort((a, b) => {
         return dayjs(b.createdAt).diff(dayjs(a.createdAt));
       })
-      .concat(legacySpacesData || [])
-      .concat(supabaseData || []);
+      .concat(legacySpacesData || []);
     if (searchVal === '') {
       return sortedData;
     }
     return sortedData?.filter((space) =>
       space.name.toLowerCase().includes(searchVal.toLowerCase()),
     );
-  }, [spaces, legacySpacesData, searchVal, supabaseData]);
+  }, [spaces, legacySpacesData, searchVal]);
 
   return (
     <div className="flex flex-1 flex-col gap-[20px] p-[20px] mobile:gap-[10px] mobile:p-[20px_10px]">
@@ -106,7 +76,7 @@ const SpaceList = () => {
       />
 
       <ResponsiveGrid>
-        {isLoading || isRoleLoading || isLegacyLoading || isSupbaseDataLoading
+        {isLoading || isRoleLoading || isLegacyLoading
           ? Array.from({ length: 8 }).map((_, index) => (
               <ResponsiveGridItem key={index}>
                 <SpaceCardSkeleton autoWidth={true} key={index} />
